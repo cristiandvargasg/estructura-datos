a9de44cbdc9cf4e4e25b95cc6bb0c05f0f7a86ae
cristiandvargasg <141505547+cristiandvargasg@users.noreply.github.com>
2023-09-23 04:25:51 +0000

    tareas

---
+++ clase 6/trabajo/cliente.py
@@ -0,0 +1,27 @@
+import requests
+import random
+
+url = 'https://expert-potato-5gqx5g999x7jc7677-8000.app.github.dev/'
+
+names = ["Jeremy", "jesus", "andres", "kerly", "melissa"]
+last_names = ["sanchez", "gonzales", "giron", "cordoba", "morales"]
+items = ["Frutas", "zapatos", "ropa deportiva", "detergentes", "utiles escolares"]
+
+def generate_element():
+    first_name = random.choice(names)
+    last_name = random.choice(last_names)
+    products = [random.choice(items) for _ in range(2)]
+    return {
+        "name": f"{first_name} {last_name}",
+        "products": products
+    }
+
+for _ in range(5):
+    element_to_enqueue = generate_element()
+    response = requests.post(f'{url}/encolar', json=element_to_enqueue)
+    
+    if response.status_code == 400:
+        data = response.json()
+        print(f"Element enqueued successfully: {data}")
+    else:
+        print(f'Error in POST to /encolar: {response.status_code}')
